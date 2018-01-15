47e4a4398c03f00d7a77bc91ce503b7b6aa02a8a
Andie Rocca <rocca.andie@gmail.com>
2018-01-14 22:53:08 -0500

    final version

--- js/main.js
+++ js/main.js
@@ -56,8 +56,6 @@ d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTEgHmkVfYGNyB8cu1ncF3eZ
                 iconSize: [size, size]
             });
 
-            console.log(d);
-
             L.marker([d.lat, d.lng], {icon: myIcon}).addTo(myMap)
                 .bindPopup(d.name)
                 .on('click', function() {
@@ -110,7 +108,7 @@ function onMarkerClick(d) {
             }
 
             html += ("<img  class=\"d-block img-fluid\" src=\"" + images[i].link + "\""
-            + "style=\"height:200px;\">"
+            + "style=\"height:300px;\">"
             + "</div>");
         }
 
@@ -144,114 +142,144 @@ function getImages(albumID) {
 
 
 
-var colors = ['#4C6085', '#50C5B7', '#736CED', '#9CEC5B', '#39A0ED', '#9D96B8','#DD6E42','#0D1F2D'];
-var activities = ['Sleeping', 'Eating', 'Driving', 'Walking', 'Being Social', 'Downtime', 'Shopping', 'Nightlife'];
+// var colors = ['#4C6085', '#50C5B7', '#736CED', '#9CEC5B', '#39A0ED', '#9D96B8','#DD6E42','#0D1F2D'];
+// var activities = ['Sleeping', 'Eating', 'Driving', 'Walking', 'Being Social', 'Downtime', 'Shopping', 'Nightlife'];
 
-var bars_svg = d3.select('#bars svg');
-var barsWidth = +parseInt(bars_svg.style('width'));
-var barsHeight = +parseInt(bars_svg.style('height'));
+// var bars_svg = d3.select('#bars svg');
+// var barsWidth = +parseInt(bars_svg.style('width'));
+// var barsHeight = +parseInt(bars_svg.style('height'));
 
-var pie_svg = d3.select('#pie svg');
-var pieWidth = +parseInt(pie_svg.style('width'));
-var pieHeight = +parseInt(pie_svg.style('height'));
+// var pie_svg = d3.select('#pie svg');
+// var pieWidth = +parseInt(pie_svg.style('width'));
+// var pieHeight = +parseInt(pie_svg.style('height'));
 
-var padding = {t:+50, b:+50, l:+70, r:+50};
+// var padding = {t:+50, b:+70, l:+70, r:+50};
 
-var xScale = d3.scaleLinear()
-    .domain([0,24])
-    .range([0, barsWidth-padding.l-padding.r]);
+// var xScale = d3.scaleLinear()
+//     .domain([0,24])
+//     .range([0, barsWidth-padding.l-padding.r]);
 
-var xAxis = bars_svg.append('g')
-    .attr('class', 'xAxis')
-    .attr('transform', 'translate('+[padding.l, padding.t + 10]+')')
-    .call(d3.axisTop(xScale)
-        .tickValues([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
-        .tickFormat(function(d) {return d + ':00'}))
-    .selectAll("text")
-        .attr("dx", "2em")
-        .attr("dy", "1.5em")
-        .attr("transform", "rotate(-65)");
+// var xAxis = bars_svg.append('g')
+//     .attr('class', 'xAxis')
+//     .attr('transform', 'translate('+[padding.l, padding.t + 10]+')')
+//     .call(d3.axisTop(xScale)
+//         .tickValues([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
+//         .tickFormat(function(d) {return d + ':00'}))
+//     .selectAll("text")
+//         .attr("dx", "2em")
+//         .attr("dy", "1.5em")
+//         .attr("transform", "rotate(-65)");
+
+// var sumXScale = d3.scaleLinear()
+//     .domain([0, activities.length - 1])
+//     .range([padding.l, pieWidth - padding.r]);
+
+// var sumXAxis = pie_svg.append('g')
+//     .attr('class', 'xAxis')
+//     .attr('transform', 'translate('+[(pieWidth - padding.l - padding.r)/(activities.length*2), pieHeight - padding.b]+')')
+//     .call(d3.axisBottom(sumXScale)
+//         .ticks(activities.length)
+//         .tickFormat(function(d) {return activities[d]}))
+//     .selectAll("text")
+//         .attr("dx", "-1em")
+//         .attr("dy", "0em")
+//         .attr("transform", "rotate(-65)")
+//         .attr('text-anchor', 'end');
 
 //hour data
-d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vS0U5fYMmF47RZsUfuW_psDoMnyxo_OjJO3pzHXDnkQ4HPCyn4kZMj_HAZblNFfHZGbHuEvmHWJSZn3/pub?output=csv',
-    function(d) {
-        return {
-            day: +d['Day'],
-            hour: +d['Hour'],
-            activity: +d['Activity']
-        };
-    }, function(error, dataset) {
-        if (error) {
-            console.error(error);
-            return;
-        }
-        hour_data = dataset;
+// d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vS0U5fYMmF47RZsUfuW_psDoMnyxo_OjJO3pzHXDnkQ4HPCyn4kZMj_HAZblNFfHZGbHuEvmHWJSZn3/pub?output=csv',
+//     function(d) {
+//         return {
+//             day: +d['Day'],
+//             hour: +d['Hour'],
+//             activity: +d['Activity']
+//         };
+//     }, function(error, dataset) {
+//         if (error) {
+//             console.error(error);
+//             return;
+//         }
+//         hour_data = dataset;
 
-        var yScale = d3.scaleLinear()
-            .domain([0,d3.max(hour_data, function(d) {return d.day})])
-            .range([0, barsHeight-padding.t-padding.b]);
+//         var yScale = d3.scaleLinear()
+//             .domain([0,d3.max(hour_data, function(d) {return d.day})])
+//             .range([0, barsHeight-padding.t-padding.b]);
 
-        var bars = bars_svg.selectAll('.bar').data(hour_data);
+//         var bars = bars_svg.selectAll('.bar').data(hour_data);
 
-        bars.enter().append('rect')
-            .attr('width', (barsWidth-padding.l-padding.r)/24 - 4)
-            .attr('height', (barsHeight-padding.t-padding.b)/(d3.max(hour_data, function(d) {return d.day})) - 8)
-            .attr('x', function(d) {return padding.l + xScale(d.hour)})
-            .attr('y', function(d) {return padding.t + yScale(d.day)})
-            .style('fill', function(d) {return d.activity == -1 ? '#fff' : colors[d.activity]});
+//         bars.enter().append('rect')
+//             .attr('width', (barsWidth-padding.l-padding.r)/24 - 4)
+//             .attr('height', (barsHeight-padding.t-padding.b)/(d3.max(hour_data, function(d) {return d.day})) - 8)
+//             .attr('x', function(d) {return padding.l + xScale(d.hour)})
+//             .attr('y', function(d) {return padding.t + yScale(d.day)})
+//             .style('fill', function(d) {return d.activity == -1 ? '#fff' : colors[d.activity]});
 
 
-        days_nested = d3.nest()
-            .key(function(d) {return d.day;})
-            .rollup(function(v) {return d3.sum(v, function(d) {return d.activity})})
-            .entries(hour_data);
+//         days_nested = d3.nest()
+//             .key(function(d) {return d.day;})
+//             .rollup(function(v) {return d3.sum(v, function(d) {return d.activity})})
+//             .entries(hour_data);
 
-        dateLabels = bars_svg.selectAll('.dateLabel').data(days_nested);
+//         dateLabels = bars_svg.selectAll('.dateLabel').data(days_nested);
 
-        dateLabels.enter().append('text')
-            .text(function(d) {
-                    return d.value > -24 ? "Jan " + (parseInt(d.key) + 14) + ", 2018" : "";
-                })
-            .attr('text-anchor', 'end')
-            .attr('transform', function(d) {return 'translate(' +[padding.l-10, 10 + padding.t + yScale(+d.key)]+ ')'})
-            .style('font-size', 10);
+//         dateLabels.enter().append('text')
+//             .text(function(d) {
+//                     return d.value > -24 ? "Jan " + (parseInt(d.key) + 14) + ", 2018" : "";
+//                 })
+//             .attr('text-anchor', 'end')
+//             .attr('transform', function(d) {return 'translate(' +[padding.l-10, 10 + padding.t + yScale(+d.key)]+ ')'})
+//             .style('font-size', 10);
 
-        activity_nested = d3.nest()
-            .key(function(d) {return d.activity})
-            .entries(hour_data);
+//         //excludes -1
+//         activity_nested = d3.nest()
+//             .key(function(d) {return d.activity})
+//             .entries(hour_data)
+//             .filter(function(d) {return d.key != -1});
 
-        var arc = g.selectAll('.arc')
-            .data(pie(activity_nested))
-            .enter().append('g')
-                .attr('class', 'arc');
+//         console.log(d3.max(activity_nested, function(d) {return d.values.length}));
 
-        arc.append('path')
-            .attr('d', path)
-            .attr('fill', function(d) {return parseInt(d.data.key) == -1 ? '#fff' : colors[parseInt(d.data.key)]});       
-        
-        arc.append("text")
-            .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
-            .attr("dy", "0.2em")
-            .style('font-size', 9)
-            .text(function(d) { return activities[parseInt(d.data.key)];})
-            .attr("text-anchor", "middle");
+//         var sumYScale = d3.scaleLinear()
+//             .domain([0, d3.max(activity_nested, function(d) {return d.values.length})])
+//             .range([padding.b, pieHeight - padding.t - padding.b]);
 
-    });
+//         yScaleAxis = d3.scaleLinear()
+//             .domain([0, d3.max(activity_nested, function(d) {return d.values.length})])
+//             .range([pieHeight - padding.t, padding.b]);
 
-middle = Math.min(pieWidth, pieHeight) / 2;
+//         var sumYAxis = pie_svg.append('g')
+//             .attr('class', 'yAxis')
+//             .attr('transform', 'translate('+[padding.l,-padding.t + 30]+')')
+//             .call(d3.axisLeft(yScaleAxis))
+//                 //.tickFormat(function(d) {d + ' hours'}))
+//             .selectAll("text")
+//                 .attr('text-anchor', 'end');
 
-var g = pie_svg.append('g').attr('transform', 'translate(' + [middle,middle+30] + ')');
 
-var pie = d3.pie()
-    .sort(null)
-    .value(function(d) { return d.key == -1 ? 0 : d.values.length; });
+//         barsSum = pie_svg.selectAll('.sumBar').data(activity_nested);
 
-var radius = Math.min(pieWidth, pieHeight) / 2;
+//         barsSum.enter().append('rect')
+//             .attr('width', (pieWidth - padding.l - padding.r)/activities.length)
+//             .attr('height', function(d) {console.log(d.values.length);return sumYScale(d.values.length)})
+//             .attr('x', function(d) {return sumXScale(d.key)})
+//             .attr('y', function(d) {return pieHeight - padding.b - sumYScale(d.values.length)})
+//             .attr('fill', function(d) {return colors[parseInt(d.key)]});
 
-var path = d3.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
+//     });
 
-var label = d3.arc()
-    .outerRadius(radius)
-    .innerRadius(radius);
+// middle = Math.min(pieWidth, pieHeight) / 2;
+
+// var g = pie_svg.append('g').attr('transform', 'translate(' + [middle,middle+30] + ')');
+
+// var pie = d3.pie()
+//     .sort(null)
+//     .value(function(d) { return d.key == -1 ? 0 : d.values.length; });
+
+// var radius = Math.min(pieWidth, pieHeight) / 2;
+
+// var path = d3.arc()
+//     .outerRadius(radius - 10)
+//     .innerRadius(0);
+
+// var label = d3.arc()
+//     .outerRadius(radius)
+//     .innerRadius(radius);
