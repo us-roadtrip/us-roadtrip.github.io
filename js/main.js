2b39eae90522a1a8a902e3f8f5e65898f283c89f
Andie Rocca <rocca.andie@gmail.com>
2018-01-21 13:13:39 -0500

    min zoom

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var myMap = L.map('map').setView(centerLatLng, 4);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 10,
-    minZoom: 1,
+    minZoom: 5,
     id: 'mapbox.light',
     accessToken: 'pk.eyJ1IjoiamFnb2R3aW4iLCJhIjoiY2lnOGQxaDhiMDZzMXZkbHYzZmN4ZzdsYiJ9.Uwh_L37P-qUoeC-MBSDteA'
 }).addTo(myMap);
